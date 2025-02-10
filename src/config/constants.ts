e034c8b9c78d94be94b17466c5f5fa72f49b52c5
martin0995 <martin@papermoon.io>
2025-02-10 14:21:45 +0100

    modularize constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,3 +3,4 @@ export const CORE = '0x98f3c9e6E3fAce36bAAd05FE09d375Ef1464288B'.toLowerCase();
 export const LOG_MESSAGE_PUBLISHED_TOPIC =
 	'0x6eb224fb001ed210e379b335e35efe88672a8ce935d981a6896b27ffdf52a3b2';
 export const WORMHOLE_SCAN_API = 'https://api.wormholescan.io/v1';
+export const TXS = ['0x3ad91ec530187bb2ce3b394d587878cd1e9e037a97e51fbc34af89b2e0719367'];
