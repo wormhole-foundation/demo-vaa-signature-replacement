14a10c724e86614c55fe4129639fc457ed2e2b43
martin0995 <martin@papermoon.io>
2025-02-10 14:49:52 +0100

    export response decoder

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,4 +3,5 @@ export { fetchVaaData } from './fetchVaaData';
 export { checkVaaValidity } from './checkVaaValidity';
 export { fetchObservations } from './fetchObservations';
 export { fetchGuardianSet } from './fetchGuardianSet';
+export { decodeResponse } from './responseDecoder';
 export * from './replaceSignatures';
