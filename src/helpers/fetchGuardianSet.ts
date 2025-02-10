b7153946b687788e68b83bd5049f99a023b7f887
martin0995 <martin@papermoon.io>
2025-02-10 14:11:56 +0100

    remove logs

--- src/helpers/fetchGuardianSet.ts
+++ src/helpers/fetchGuardianSet.ts
@@ -4,12 +4,11 @@ import { WORMHOLE_SCAN_API } from '../config';
 // ✅ Fetch current guardian set
 export async function fetchGuardianSet() {
 	try {
-		console.log('🛠 Fetching current guardian set...');
+		console.log('🛠  Fetching current guardian set');
 		const response = await axios.get(`${WORMHOLE_SCAN_API}/guardianset/current`);
-		// console.log('✅ Guardian set fetched:', response.data);
-
 		const guardians = response.data.guardianSet.addresses.map((addr: string) => addr.toLowerCase());
 		const guardianSet = response.data.guardianSet.index;
+
 		return [guardians, guardianSet];
 	} catch (error) {
 		console.error('❌ Error fetching guardian set:', error);
