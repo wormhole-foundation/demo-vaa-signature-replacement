d2de56a5540abb99bf812096c91938f3ae0fff47
martin0995 <martin@papermoon.io>
2025-02-14 19:33:09 +0100

    update export

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,7 @@
-export { fetchVaaIds } from './fetchVaaIds';
-export { fetchVaaData } from './fetchVaaData';
-export { checkVaaValidity } from './checkVaaValidity';
-export { fetchObservations } from './fetchObservations';
-export { fetchGuardianSet } from './fetchGuardianSet';
-export { decodeResponse } from './responseDecoder';
+export * from './fetchVaaIds';
+export * from './fetchVaaData';
+export * from './checkVaaValidity';
+export * from './fetchObservations';
+export * from './fetchGuardianSet';
+export * from './responseDecoder';
 export * from './replaceSignatures';
