33b6852be875083a6742092f4e11f05dba9aa8f7
martin0995 <martin@papermoon.io>
2025-02-17 16:11:06 +0100

    fetch vaaids for solana

--- src/helpers/fetchVaaIds.ts
+++ src/helpers/fetchVaaIds.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
-import { RPC, CORE, LOG_MESSAGE_PUBLISHED_TOPIC } from '../config/constants';
+import {
+	RPC,
+	CORE,
+	LOG_MESSAGE_PUBLISHED_TOPIC,
+	RPC_SOL,
+	SOL_EMITTER_ADDRESS,
+} from '../config/constants';
 
 // Function to fetch VAA IDs from Ethereum transaction receipts
 export async function fetchVaaIds(txHashes: string[]): Promise<string[]> {
@@ -25,6 +31,43 @@ export async function fetchVaaIds(txHashes: string[]): Promise<string[]> {
 					vaaIds.push(`2/${emitter}/${seq}`);
 				}
 			}
+		} catch (error) {
+			console.error(`❌ Error processing ${tx}:`, error);
+		}
+	}
+
+	return vaaIds;
+}
+
+// Function to fetch VAA IDs from Solana transaction logs
+export async function fetchSolanaVaaIds(txHashes: string[]): Promise<string[]> {
+	const vaaIds: string[] = [];
+
+	for (const tx of txHashes) {
+		try {
+			const result = (
+				await axios.post(RPC_SOL, {
+					jsonrpc: '2.0',
+					id: 1,
+					method: 'getTransaction',
+					params: [tx, 'json'],
+				})
+			).data.result;
+
+			if (!result) throw new Error(`Unable to fetch transaction details for ${tx}`);
+
+			let sequence = null;
+
+			// Extract sequence number from logs
+			for (const log of result.meta.logMessages || []) {
+				const sequenceMatch = log.match(/Program log: Sequence: (\d+)/);
+				if (sequenceMatch) {
+					sequence = sequenceMatch[1];
+					break;
+				}
+			}
+
+			vaaIds.push(`1/${SOL_EMITTER_ADDRESS}/${sequence}`);
 		} catch (error) {
 			console.error(`❌ Error processing ${tx}:`, error);
 		}
